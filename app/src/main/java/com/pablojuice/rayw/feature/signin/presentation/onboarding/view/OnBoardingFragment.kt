eaaa3818bb7e7ac751550b2551ef484c1ead5dd4
PabloJuice <pablojuice.dev@gmail.com>
2023-03-05 15:28:12 +0100

    Onboarding with animations

--- app/src/main/java/com/pablojuice/rayw/feature/signin/presentation/onboarding/view/OnBoardingFragment.kt
+++ app/src/main/java/com/pablojuice/rayw/feature/signin/presentation/onboarding/view/OnBoardingFragment.kt
@@ -1,17 +1,21 @@
 package com.pablojuice.rayw.feature.signin.presentation.onboarding.view
 
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.pablojuice.core.presentation.animation.AnimationConstants.SHORT_ANIMATION_TIME
 import com.pablojuice.core.presentation.animation.list.AlphaPageTransformer
 import com.pablojuice.core.presentation.basic.BasicFragment
 import com.pablojuice.core.presentation.view.setVisible
 import com.pablojuice.rayw.databinding.FragmentOnboardingBinding
 import com.pablojuice.rayw.feature.signin.presentation.onboarding.list.OnBoardingAdapter
 import dagger.hilt.android.AndroidEntryPoint
+
+private const val PAGE_PROGRESS_SCALE = 100
 
 @AndroidEntryPoint
 class OnBoardingFragment : BasicFragment<FragmentOnboardingBinding, OnBoardingViewModel>() {
@@ -21,7 +25,7 @@ class OnBoardingFragment : BasicFragment<FragmentOnboardingBinding, OnBoardingVi
     override fun bindLayout(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        attachToParent: Boolean
+        attachToParent: Boolean,
     ) = FragmentOnboardingBinding.inflate(inflater, container, attachToParent)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -52,15 +56,29 @@ class OnBoardingFragment : BasicFragment<FragmentOnboardingBinding, OnBoardingVi
         binding.viewPager.adapter = adapter
         binding.viewPager.setPageTransformer(AlphaPageTransformer())
         addOnPageChangeCallback(adapter)
+        binding.progressBar.max = adapter.itemCount * PAGE_PROGRESS_SCALE
     }
 
     private fun addOnPageChangeCallback(adapter: OnBoardingAdapter) {
         binding.viewPager.registerOnPageChangeCallback(object : OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
+                animateProgressBar(position)
                 binding.previous.setVisible(position > 0)
                 binding.next.setVisible(position < adapter.itemCount - 1)
                 binding.rentOut.setVisible(position == adapter.itemCount - 1)
             }
         })
+    }
+
+    private fun animateProgressBar(position: Int) {
+        val animator = ValueAnimator.ofInt(
+            binding.progressBar.progress,
+            (position + 1) * PAGE_PROGRESS_SCALE,
+        )
+        animator.duration = SHORT_ANIMATION_TIME
+        animator.addUpdateListener {
+            binding.progressBar.progress = Integer.parseInt(it.animatedValue.toString())
+        }
+        animator.start()
     }
 }
---
+++ core/src/main/java/com/pablojuice/core/presentation/animation/AnimationConstants.kt
@@ -0,0 +1,7 @@
+package com.pablojuice.core.presentation.animation
+
+object AnimationConstants {
+    const val SHORT_ANIMATION_TIME = 200L
+    const val MEDIUM_ANIMATION_TIME = 400L
+    const val LONG_ANIMATION_TIME = 500L
+}
