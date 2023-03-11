ff582ddd00cade8c023da378cb911de7a66b2523
PabloJuice <pablojuice.dev@gmail.com>
2023-03-11 21:36:07 +0100

    Animation and ui fixes

--- core/src/main/java/com/pablojuice/core/presentation/animation/AnimationConstants.kt
+++ core/src/main/java/com/pablojuice/core/presentation/animation/AnimationConstants.kt
@@ -1,7 +1,8 @@
 package com.pablojuice.core.presentation.animation
 
 object AnimationConstants {
-    const val SHORT_ANIMATION_TIME = 200L
+    const val EXTRA_SHORT_ANIMATION_TIME = 200L
+    const val SHORT_ANIMATION_TIME = 300L
     const val MEDIUM_ANIMATION_TIME = 400L
     const val LONG_ANIMATION_TIME = 500L
 }
