a9b64b2bb8828a71f89e4d2e73ef04ce9612d086
PabloJuice <pablojuice.dev@gmail.com>
2023-05-17 22:39:14 +0200

    rent details added

--- app/src/main/java/com/pablojuice/rayw/feature/rent_list/data/repository/RentRepository.kt
+++ app/src/main/java/com/pablojuice/rayw/feature/rent_list/data/repository/RentRepository.kt
@@ -9,6 +9,13 @@ import com.pablojuice.rayw.feature.rent_list.data.remote.RentListData
 import dagger.Reusable
 import javax.inject.Inject
 
+private const val DESCRIPTION =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet quam vel finibus consectetur. Etiam molestie neque nulla, eget lobortis nulla facilisis eget. Curabitur tellus neque, sodales nec posuere id, accumsan id nisi. Pellentesque vitae enim convallis, commodo mi accumsan, dapibus nunc. Suspendisse nec nunc iaculis, aliquam ipsum vitae, laoreet leo. Nunc et eros at enim mollis sodales. Quisque porta turpis et facilisis dictum. Nullam in mattis nibh. Vivamus rhoncus quis quam ut eleifend. Sed lectus tellus, luctus ac velit vel, lobortis congue metus.\n" +
+            "\n" +
+            "Curabitur lobortis ligula turpis, nec vulputate ex ornare ac. Aenean imperdiet in elit in euismod. Etiam faucibus vehicula purus, sit amet euismod velit pretium auctor. Mauris feugiat turpis mauris, ut vestibulum elit maximus a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In varius imperdiet lacus sit amet ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate nisl non purus pharetra egestas. Aliquam et ligula est. Ut commodo metus vel laoreet imperdiet. Ut ultricies ut nisl pretium volutpat. Aliquam pretium, urna porttitor scelerisque convallis, ante quam dapibus magna, ac pharetra magna ligula luctus enim.\n" +
+            "\n" +
+            "Vivamus a risus lacinia, faucibus ligula quis, porttitor ligula. Fusce dictum nisi sodales dolor volutpat faucibus. Nulla nunc nunc, pharetra ac purus nec, facilisis consectetur lectus. Vivamus quis rhoncus elit. Ut eu nulla vehicula, imperdiet nisl non, laoreet metus. Vestibulum vitae quam vestibulum, rhoncus ipsum et, faucibus nulla. Aenean at elit eget augue luctus commodo. Fusce ac vehicula augue. Cras tincidunt posuere cursus. Curabitur imperdiet risus vel dolor ornare hendrerit."
+
 @Reusable
 class RentRepository @Inject constructor(
     private val userPreferences: UserPreferences
@@ -148,7 +155,7 @@ class RentRepository @Inject constructor(
                     R.drawable.ic_mock_bike
                 ),
                 title = "Велосипед Україна",
-                description = "Велосипед Україна",
+                description = DESCRIPTION,
                 isInWishList = wishlist.any { it == id },
                 price = 300.0,
                 priceCurrency = "Грн",
@@ -169,7 +176,7 @@ class RentRepository @Inject constructor(
                     R.drawable.ic_mock_ps_6,
                 ),
                 title = "Приставка PlayStation 4 250GB з іграми",
-                description = "Приставка PlayStation 4 250GB з іграми",
+                description = DESCRIPTION,
                 isInWishList = wishlist.any { it == id },
                 price = 199.0,
                 priceCurrency = "Грн",
@@ -187,7 +194,7 @@ class RentRepository @Inject constructor(
                     R.drawable.ic_mock_projector
                 ),
                 title = "Проектор SAMSUNG 4k",
-                description = "Проектор SAMSUNG 4k",
+                description = DESCRIPTION,
                 isInWishList = wishlist.any { it == id },
                 price = 350.0,
                 priceCurrency = "Грн",
@@ -205,7 +212,7 @@ class RentRepository @Inject constructor(
                     R.drawable.ic_mock_book
                 ),
                 title = "Книга Чистий код (Роберт Мартін)",
-                description = "Книга Чистий код (Роберт Мартін)",
+                description = DESCRIPTION,
                 isInWishList = wishlist.any { it == id },
                 price = 59.0,
                 priceCurrency = "Грн",
@@ -226,7 +233,7 @@ class RentRepository @Inject constructor(
                     R.drawable.ic_mock_tool_6
                 ),
                 title = "Прямий перфоратор Bosch GBH 2-26 DRE Professional",
-                description = "Прямий перфоратор Bosch GBH 2-26 DRE Professional",
+                description = DESCRIPTION,
                 isInWishList = wishlist.any { it == id },
                 price = 200.0,
                 priceCurrency = "Грн",
@@ -244,7 +251,7 @@ class RentRepository @Inject constructor(
                     R.drawable.ic_mock_mic
                 ),
                 title = "Студійний мікрофон Fifine k669 pro-1",
-                description = "Студійний мікрофон Fifine k669 pro-1",
+                description = DESCRIPTION,
                 isInWishList = wishlist.any { it == id },
                 price = 349.0,
                 priceCurrency = "Грн",
@@ -262,7 +269,7 @@ class RentRepository @Inject constructor(
                     R.drawable.ic_mock_cam
                 ),
                 title = "Фотокамера Canon 6D",
-                description = "Фотокамера Canon 6D",
+                description = DESCRIPTION,
                 isInWishList = wishlist.any { it == id },
                 price = 250.0,
                 priceCurrency = "Грн",
@@ -280,7 +287,7 @@ class RentRepository @Inject constructor(
                     R.drawable.ic_mock_generator
                 ),
                 title = "Бензиновий генератор Al-ko 6500D-C 5.5кВт",
-                description = "Бензиновий генератор Al-ko 6500D-C 5.5кВт",
+                description = DESCRIPTION,
                 isInWishList = wishlist.any { it == id },
                 price = 449.0,
                 priceCurrency = "Грн",
--- app/src/main/java/com/pablojuice/rayw/feature/rent_list/presentation/details/view/RentDetailsFragment.kt
+++ app/src/main/java/com/pablojuice/rayw/feature/rent_list/presentation/details/view/RentDetailsFragment.kt
@@ -1,7 +1,11 @@
 package com.pablojuice.rayw.feature.rent_list.presentation.details.view
 
 import androidx.fragment.app.viewModels
+import androidx.viewpager2.widget.ViewPager2
+import com.pablojuice.core.presentation.view.animation.list.AlphaPageTransformer
 import com.pablojuice.core.presentation.view.fragment.BasicFragment
+import com.pablojuice.core.presentation.view.label.setLabel
+import com.pablojuice.core.presentation.view.setVisible
 import com.pablojuice.core.presentation.view.toolbar.setNavigationClickListener
 import com.pablojuice.rayw.databinding.FragmentRentDetailsBinding
 import com.pablojuice.rayw.feature.rent_list.data.local.RentDetailsItem
@@ -18,16 +22,46 @@ class RentDetailsFragment : BasicFragment<FragmentRentDetailsBinding, RentDetail
 
     override fun setupScreen() {
         binding.itemToolBar.setNavigationClickListener(::navigateBack)
+        setupListeners()
         viewModel.itemDetails.observe(::setItemDetails)
         arguments?.let { args ->
             viewModel.fetchDetailsForItem(RentDetailsFragmentArgs.fromBundle(args).rentId)
         }
     }
 
+    private fun setupListeners() {
+        binding.next.setOnClickListener {
+            val currentPosition = binding.imagePager.currentItem
+            if (currentPosition < binding.imagePager.adapter!!.itemCount - 1) {
+                binding.imagePager.setCurrentItem(currentPosition + 1, true)
+            }
+        }
+        binding.previous.setOnClickListener {
+            val currentPosition = binding.imagePager.currentItem
+            if (currentPosition > 0) {
+                binding.imagePager.setCurrentItem(currentPosition - 1, true)
+            }
+        }
+        binding.imagePager.setPageTransformer(AlphaPageTransformer())
+        binding.imagePager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                binding.previous.setVisible(position > 0)
+                binding.next.setVisible(position < binding.imagePager.adapter!!.itemCount - 1)
+            }
+        })
+    }
+
     private fun setItemDetails(item: RentDetailsItem?) {
         item?.run {
             binding.imagePager.adapter = RentDetailsImageAdapter(icon.map { RentDetailsImage(it) })
-
+            binding.dateLabel.setLabel(timeAdded)
+            binding.titleLabel.setLabel(title)
+            binding.ratingLabel.setLabel(rating)
+            binding.priceLabel.setLabel(price)
+            binding.currencyLabel.setLabel(priceCurrency)
+            binding.priceDescriptionLabel.setLabel(priceDescription)
+            binding.descriptionLabel.setLabel(description)
+            binding.locationLabel.setLabel(location)
         }
     }
 }
