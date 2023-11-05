1d7e7b54d5c456a2bf1f566d5317ae081427a57f
PabloJuice <pablojuice.dev@gmail.com>
2023-11-05 10:46:13 +0100

    preferences optimization

--- feature/preferences/src/main/java/com/pablojuice/rayw/feature/preferences/presentation/details/PreferencesAppSettingsFragment.kt
+++ feature/preferences/src/main/java/com/pablojuice/rayw/feature/preferences/presentation/details/PreferencesAppSettingsFragment.kt
@@ -1,10 +1,10 @@
 package com.pablojuice.rayw.feature.preferences.presentation.details
 
-import android.widget.EditText
 import androidx.fragment.app.viewModels
-import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import com.pablojuice.core.presentation.view.fragment.BasicFragment
-import com.pablojuice.core.presentation.view.text.Label
+import com.pablojuice.core.presentation.view.text.setMenuItemClickListener
+import com.pablojuice.core.presentation.view.text.setMenuItems
+import com.pablojuice.core.presentation.view.text.setMenuSelectedItem
 import com.pablojuice.core.presentation.view.toolbar.setNavigationClickListener
 import com.pablojuice.rayw.feature.preferences.databinding.FragmentPreferencesAppSettingsBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,26 +19,11 @@ class PreferencesAppSettingsFragment :
 
     override fun setupScreen() {
         binding.preferencesToolBar.setNavigationClickListener(::navigateBack)
-        binding.languageInput.editText.setItems(viewModel.getAvailableLanguages())
-        binding.themeInput.editText.setItems(viewModel.getAvailableThemes())
-        binding.languageInput.editText.setOnItemClickListener(viewModel::setAppLanguage)
-        binding.themeInput.editText.setOnItemClickListener(viewModel::setAppTheme)
-        viewModel.appLanguage.observe { binding.languageInput.editText.setSelectedItem(it.displayName) }
-        viewModel.appTheme.observe { binding.themeInput.editText.setSelectedItem(it.displayName) }
-    }
-
-    private fun EditText?.setItems(items: List<Label>) {
-        val transformedItems = items.map { it.get(requireContext()) }.toTypedArray()
-        (this as MaterialAutoCompleteTextView).setSimpleItems(transformedItems)
-    }
-
-    private fun EditText?.setOnItemClickListener(onClick: (Int) -> Unit) {
-        (this as MaterialAutoCompleteTextView).setOnItemClickListener { _, _, position, _ ->
-            onClick(position)
-        }
-    }
-
-    private fun EditText?.setSelectedItem(item: Label) {
-        (this as MaterialAutoCompleteTextView).setText(item.get(context), false)
+        binding.languageInput.editText?.setMenuItems(viewModel.getAvailableLanguages())
+        binding.themeInput.editText?.setMenuItems(viewModel.getAvailableThemes())
+        binding.languageInput.editText?.setMenuItemClickListener(viewModel::setAppLanguage)
+        binding.themeInput.editText?.setMenuItemClickListener(viewModel::setAppTheme)
+        viewModel.appLanguage.observe { binding.languageInput.editText?.setMenuSelectedItem(it.displayName) }
+        viewModel.appTheme.observe { binding.themeInput.editText?.setMenuSelectedItem(it.displayName) }
     }
 }
